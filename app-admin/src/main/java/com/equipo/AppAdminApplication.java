cf98ee8c41f4196238fb66b511d219fd790ff6f1
SalvadorTrincado <salvador.trincado@educa.madrid.org>
2025-04-17 01:14:10 +0200

    Creación de módulos app-empleados y app-admin

--- app-admin/src/main/java/com/equipo/AppAdminApplication.java
+++ app-admin/src/main/java/com/equipo/AppAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontendApplication {
+public class AppAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrontendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppAdminApplication.class, args);
+    }
 
 }
