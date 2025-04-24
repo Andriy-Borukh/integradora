3043266aadec8a7653ffa8dc1d4f6851c1c02d0b
Andriy_Borukh <ch1dryx@gmail.com>
2025-04-24 13:12:25 +0200

    Integradora

---
+++ common-lib/src/test/java/com/equipo/commonlib/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.commonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
